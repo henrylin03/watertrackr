64e29c11a2813d8426c6e18732d8cd4ec385a295
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-08-12 19:40:53 +1000

    create react template with vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Hello World</p>
+      <Button />
+    </>
+  );
+}
+
+export default App;
